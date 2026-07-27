e3d6cf3f134336577f0e6d3bf60712f5fee555d1
XiaoZzya <88625758+XiaoZzya@users.noreply.github.com>
2026-07-28 03:33:22 +0800

    build24.sh

--- x86-64/build24.sh
+++ x86-64/build24.sh
@@ -52,10 +52,16 @@ PACKAGES="$PACKAGES luci-i18n-firewall-zh-cn"
 PACKAGES="$PACKAGES luci-theme-argon"
 PACKAGES="$PACKAGES luci-app-argon-config"
 PACKAGES="$PACKAGES luci-i18n-argon-config-zh-cn"
+PACKAGES="$PACKAGES luci-app-openclash"
+PACKAGES="$PACKAGES luci-app-smartdns"
+PACKAGES="$PACKAGES luci-app-ddns"
+
 #24.10
 PACKAGES="$PACKAGES luci-i18n-package-manager-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-ttyd-zh-cn"
 PACKAGES="$PACKAGES openssh-sftp-server"
+PACKAGES="$PACKAGES ddns-go"
+PACKAGES="$PACKAGES WireGuard"
 
 # 文件管理器
 PACKAGES="$PACKAGES luci-i18n-filemanager-zh-cn"
